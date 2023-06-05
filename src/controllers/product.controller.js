89e6fd2a997340fa4104d3314d5c7fc3de43c865
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2023-06-05 17:59:16 +0530

    added nodemon and added route for product details

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -5,10 +5,19 @@ const Product = require('../models/product.model');
 router.get('/list',async(req,res)=>{
     try {
         const page = req.query.page ||1;
-        const size = req.query.size || 3;
+        const size = req.query.size || 10;
         const skip = (page-1)*size;
-        const products = await Product.find({},{id:1,item_name:1,item_image:1,item_price:1}).skip(skip).limit(size).lean().exec()
+        const products = await Product.find({},{id:1,item_name:1,item_image:1,item_price:1,_id:0}).skip(skip).limit(size);
         return res.status(200).send(products);
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+});
+
+router.get('/:id',async(req,res)=>{
+    try {
+        const product = await Product.find({id:req.params.id},{_id:0});
+        return res.status(200).send(product);
     } catch (err) {
         return res.status(500).send(err.message)
     }
